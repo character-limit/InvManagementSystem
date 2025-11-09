f51b3326f1dc459382ce95f6720cd16620b6ecda
character-limit <lukekello18@gmail.com>
2025-11-09 23:46:03 +0000

    fix

--- main.py
+++ main.py
@@ -3,42 +3,6 @@ from datetime import datetime
 from item import Item
 from user import User
 
-def main():
-    """ items = Item.load_items()
-
-    for i in items:
-        print(i)
-
-
-    temp = Item("testname", 40, "sotragename", "0005", datetime.now().strftime("%d/%m/%Y %H:%M"))
-
-    items.append(temp)
-
-    Item.write_items(items)
-
-    for i in items:
-        print(i)
-
-
-    User.load_users()
-
-    print(User.users)
-
-    User.create_user(
-        input ("first Name: "),
-        input ("last Name: "),
-        input ("user Name: "),
-        input ("password: ")
-    )
-
-    print("yes")
-
-    print("LOGIN")
-
-    username = input("username: ")
-    password = input("password: ")
-
-    User.login(username, password) """
 
 def title_page():
     os.system('cls' if os.name == 'nt' else 'clear')
